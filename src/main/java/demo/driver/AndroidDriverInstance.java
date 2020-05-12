c1989896c41192f8fabac8056833e708505b047c
el-03 <bilalelbari@gmail.comclear>
2020-05-12 15:37:35 +0700

    Update local driverInstance

--- src/main/java/demo/driver/AndroidDriverInstance.java
+++ src/main/java/demo/driver/AndroidDriverInstance.java
@@ -2,6 +2,8 @@ package demo.driver;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -17,11 +19,14 @@ public class AndroidDriverInstance {
         String appiumUrl = "http://127.0.0.1:4723/wd/hub/";
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "8.1.0");
+        caps.setCapability("platformVersion", "8.0.0");
         caps.setCapability("deviceName", "device");
         caps.setCapability("udid", "emulator-5554");
-        caps.setCapability("app", "/Users/arderitoe/IdeaProjects/sample-selenium-exam/APP/TodoApp.apk");
-        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("autoGrantPermissions", true);
+        caps.setCapability("newCommandTimeout", 120);
+        caps.setCapability("app",
+                System.getProperty("user.dir") + File.separator + "APP" + File.separator + "mokapos.apk");
+        caps.setCapability("automationName", "UIAutomator2");
         try {
             androidDriver = new AndroidDriver<>(new URL(appiumUrl), caps);
             androidDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
